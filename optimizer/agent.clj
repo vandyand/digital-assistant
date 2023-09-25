e14ba0dbab72601ba994efadeff94377fe495a79
vandyand <vandyand@gmail.com>
2023-09-25 17:37:47 -0400

    refactoring agent.clj for usability

--- optimizer/agent.clj
+++ optimizer/agent.clj
@@ -6,51 +6,64 @@
   (json/read-str (response :body) :key-fn keyword))
 
 (defn post-gpt 
-  ([query-params]
-   (println query-params)
+  ([messages query-params]
+   (println "post-gpt args" messages query-params)
   (let [url "https://api.openai.com/v1/chat/completions"
         headers {"Content-Type" "application/json"
                  "Authorization" (str "Bearer " (System/getenv "OPENAI_API_KEY"))}
         body {:model (:model query-params)
-              :messages [{:role "user" :content (:query query-params)}]
+              :messages messages
               :temperature (:temperature query-params)
-              :max_tokens (:max_tokens query-params)}
+              :max_tokens (:max_tokens query-params)
+              :n (:n query-params)}
         json-body (json/write-str body)
         byte-array-body (.getBytes json-body "UTF-8")]
     (http/post url {:body byte-array-body
                     :headers headers}))))
-(defn p+ [x]
-  x
-  (do
-    (println x)
-    x))
 
-(defn get-query-params 
-  ([query] (get-query-params query "gpt-3.5-turbo"))
-  ([query model] (get-query-params query model 1.2))
-  ([query model temperature] (get-query-params query model temperature 256))
-  ([query model temperature max-tokens] (get-query-params query model temperature max-tokens 1))
-  ([query model temperature max-tokens n]
-  {:model model
-   :query query
-;;    :messages [{:role "user" :content query}]
-   :temperature temperature
-   :max_tokens max-tokens
-;;    :n n
-   }))
+(def default-gpt-params 
+  {:model "gpt-3.5-turbo"
+   :temperature 0.7
+   :max_tokens 100
+   :n 1})
 
-(defn *query-gpt [query-params] 
-  (-> (post-gpt query-params)
+(defn get-first-message-content [response]
+  (-> response
       parse-response-body
       :choices
       first
       :message
       :content
-    ;;   p+
       ))
 
-(defn query-gpt [& args]
-  (*query-gpt (apply get-query-params args)))
+(defn build-message [role content]
+  {:role role
+   :content content})
 
-(let [x "What did the cyclopse say to the upside-down turtle?"]
-  (query-gpt x "gpt-4" 0.5))
+
+(def default-messages
+  [(build-message "system" "you are a helpful agent.")
+   (build-message "user" "what is the square root of -1?")])
+
+(defn get-roles-vec [n]
+  (vec (cons "system" (take (dec n) (cycle ["user" "assistant"])))))
+
+(defn build-messages [& messages]
+  (let [n (count messages)
+        roles (get-roles-vec n)]
+  (mapv #(build-message %1 %2) roles messages)))
+
+(defn build-gpt-params [model temperature max-tokens n]
+  {:model model
+   :temperature temperature
+   :max_tokens max-tokens
+   :n n})
+
+(defn reason-unit [messages gpt-params]
+  (parse-response-body (post-gpt messages gpt-params)))
+
+#_(reason-unit default-messages default-gpt-params)
+
+#_(reason-unit
+   (build-messages "sys message" "what up broooooo?" "hey you know me man. how's it going?" "yeah yeah I need your help with something" "ok, shoot bro" "ok, what's the koochies kooch you've ever done?")
+   default-gpt-params)
